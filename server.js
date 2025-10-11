1a54e99b0fa9f63a5dab2ba7bdc6b3102d02d1a3
letsgetcreative <letsgetcreative@myahoo.com>
2025-10-11 09:47:06 -0600

    DEBUG: Add console logs to OAuth callback route to verify it's being hit

--- server.js
+++ server.js
@@ -4178,6 +4178,8 @@ app.options('/api/customer/auth/google', (req, res) => {
 // Handle Google OAuth callback (when user returns from Google)
 console.log('📝 Registering OAuth callback route: GET /oauth/callback');
 app.get('/oauth/callback', async (req, res) => {
+  console.log('🔐 OAuth callback HIT - Route is working!');
+  console.log('Query params:', req.query);
   try {
     logger.info('🔐 OAuth callback received', { query: req.query });
     const { code, state } = req.query;
